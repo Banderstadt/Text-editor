0839e8b1860de26d279741a28cdad99df9b7d2d0
Vasyl Zadorozhnyi <amdpaxton@gmail.com>
2017-04-25 02:10:38 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -2,203 +2,190 @@ getId = function (id) {
     return document.getElementById(id);
 }
 
-function f1() {
-    for (var i = 0; i < 4; i++) {
-        if (getId('changeFont')[i].selected) {
-            getId('box1').style.fontFamily = getId('changeFont')[i].value;
-        }
-    }
-}
-
-function f2(x) {
-    getId('box1').style.fontSize = x + "px";
-}
-
 var counter = false;
 
-function editText() {
+function boldText() {
     if (counter == false) {
-        getId('box2Text').style.visibility = "visible";
+        getId("content").style.fontWeight = "bold";
         counter = true;
     } else {
-        getId('box2Text').style.visibility = "hidden";
-        counter = false;
-    }
-    getId('box2Text').value = getId('box1').innerHTML;
-}
-
-function saveText() {
-    getId('box1').innerHTML = getId('box2Text').value;
-}
-
-var counter1 = false;
-
-function textColor() {
-    if (counter1 == false) {
-        getId('colorsText').style.display = "flex";
-        getId('colorsBg').style.display = "none";
-        counter1 = true;
-    } else {
-        if (counter1 == true) {
-            getId('colorsText').style.display = "none";
-            getId('colorsBg').style.display = "none";
-            counter1 = false;
+        if (counter == true) {
+            getId("content").style.fontWeight = "normal";
+            counter = false;
         }
     }
-    for (var i = 0; i < 18; i++) {
-        document.querySelectorAll('.box')[i].onclick = function () {
-            getId('box1').style.color = this.style.backgroundColor;
-        }
-
-    }
 }
+
 var counter2 = false;
 
-function fontColor() {
+function italicText() {
     if (counter2 == false) {
-        getId('colorsBg').style.display = "flex";
-        getId('colorsText').style.display = "none";
+        getId("content").style.fontStyle = "italic";
         counter2 = true;
     } else {
         if (counter2 == true) {
-            getId('colorsBg').style.display = "none";
-            getId('colorsText').style.display = "none";
+            getId("content").style.fontStyle = "normal";
             counter2 = false;
+        }
+    }
+}
+
+var counter3 = false;
+
+function underlineText() {
+    if (counter3 == false) {
+        getId("content").style.textDecoration = "underline";
+        counter3 = true;
+    } else {
+        if (counter3 == true) {
+            getId("content").style.textDecoration = "none";
+            counter3 = false;
+        }
+    }
+}
+
+function changeFont() {
+    for (var i = 0; i < 4; i++) {
+        if (getId('changeFont')[i].selected) {
+            getId('content').style.fontFamily = getId('changeFont')[i].value;
+        }
+    }
+}
+
+function changeSize() {
+    for (var i = 0; i < 7; i++) {
+        if (getId('changeSize')[i].selected) {
+            getId('content').style.fontSize = getId('changeSize')[i].value + "px";
+        }
+    }
+}
+
+var counter4 = false;
+
+function textColor() {
+    if (counter4 == false) {
+        getId('colorsText').style.display = "flex";
+        getId('colorsBg').style.display = "none";
+        counter4 = true;
+    } else {
+        if (counter4 == true) {
+            getId('colorsText').style.display = "none";
+            getId('colorsBg').style.display = "none";
+            counter4 = false;
         }
     }
     for (var i = 0; i < 18; i++) {
         document.querySelectorAll('.box')[i].onclick = function () {
-            getId('box1').style.background = this.style.backgroundColor;
+            getId('content').style.color = this.style.backgroundColor;
         }
 
     }
 }
 
-function boldText(x) {
-    if (x.checked) {
-        getId('box1').style.fontWeight = "bold";
+var counter5 = false;
+
+function fontColor() {
+    if (counter5 == false) {
+        getId('colorsBg').style.display = "flex";
+        getId('colorsText').style.display = "none";
+        counter5 = true;
     } else {
-        getId('box1').style.fontWeight = "normal";
-    }
-}
-
-function italicText(x) {
-    if (x.checked) {
-        getId('box1').style.fontStyle = "italic";
-    } else {
-        getId('box1').style.fontStyle = "normal";
-    }
-}
-
-
-
-//function tableCreate() {
-//    var rowValue = parseInt(getId("tableRow").value);
-//    var colValue = parseInt(getId("tableCol").value);
-//    var table = getId("box2Text");
-//    var tbl = document.createElement("table");
-//    var tblBody = document.createElement("tbody");
-//
-//    for (var i = 0; i < rowValue; i++) {
-//        var row = document.createElement("tr");
-//        for (var j = 0; j < tableCol; j++) {
-//            var cell = document.createElement("td");
-//            row.appendChild(cell);
-//        }
-//        tblBody.appendChild(row);
-//    }
-//    tbl.appendChild(tblBody);
-//    table.appendChild(tbl);
-//    tbl.setAttribute("border", "2px");
-//}
-
-
-function tableCreate() {
-    var box = getId('box2Text'),
-        tbl = document.createElement('table');
-    tbl.id = 'tbl';
-    tbl.style.width = '100%';
-    tbl.style.border = "1px  solid black";
-    for (var i = 0; i < 10; i++) {
-        var tr = tbl.insertRow();
-        tr.id = i;
-        for (var j = 0; j < 10; j++) {
-            var td = tr.insertCell();
-            var txtArr = 'Silex is a PHP microframework for PHP 5.3. It is built on the shoulders of Symfony2 and Pimple and also inspired by sinatra'.split(' ');
-            var txt = txtArr[Math.round(Math.random() * txtArr.length)];
-            var txt = typeof txt === 'undefined' ? 'undefined' : txt;
-            td.appendChild(document.createTextNode(txt))
+        if (counter5 == true) {
+            getId('colorsBg').style.display = "none";
+            getId('colorsText').style.display = "none";
+            counter5 = false;
         }
     }
-    window.tbl = tbl;
-    box.appendChild(tbl);
+    for (var i = 0; i < 18; i++) {
+        document.querySelectorAll('.box')[i].onclick = function () {
+            getId('content').style.background = this.style.backgroundColor;
+        }
+
+    }
 }
 
-//Зникнення виборру кольору тексту і фону(9-ть кольорових квадрата)
-// 
-//getE('colorText').onclick = function (e){
-// getE('color').style.display = "flex";
-// getE('fon').style.display = "none";
-// e.stopPropagation(); // - зупиняє наступні події
-// (дописуємо)}
-//
-//getE('colorFon').onclick = function (e){
-// getE('color').style.display = "none";
-// getE('fon').style.display = "flex"; 
-// e.stopPropagation(); // - зупиняє наступні події (дописуємо в дану функцію)
-//}
-//
-//document.body.onclick=function(e){
-////функція зникнення....
-// //console.log(e.target);
-// if(e.target.className!="colorBox" && e.target.className!="fonBox" ){ 
-//  //позиція мишки де відбувся клік НЕ повинна бути class"colorBox" && class"fonBox" тоді
-//  getE("color").style.display="none";
-// getE("fon").style.display="none";
-// }
-//}
+function changeAlign(x) {
+    getId("content").style.textAlign = x;
+}
 
-//функція перевірки валідності введених у форму даних
-//function f151(){
-//    for(var i=0;i<5;i++){
-//   var x = (document.forms.form5[i].value)/1;
-//    if((x==document.forms.form5[i].value) && (x!=0) && (x>0))
-//    {document.forms.form5[i].style.background="#00FF00";
-//     document.querySelectorAll("span")[i].style.display="none";
-//    }
-//       else{  document.forms.form5[i].style.background="#FF00AE";
-//document.querySelectorAll("span")[i].style.display="inline";
-//         } 
-//    }
-//}
+// стандартний текст
+getId('editText').value = getId('content').innerHTML;
 
-//+ нижче текст форми 5
-//<form name="form5" id="form5table">
-//        Введіть к-сть рядків таблиці:<input type="text" onblur="f151()" style="width: 40px;" > <span class="eror1" >Некоректні дані!!!</span>
-//        <br><br>
-//        Введіть к-сть стовбців таблиці: <input type="text" style="width: 40px;" onblur="f151()"><span class="eror1" >Некоректні дані!!!</span>  
-//        
-//        <p>Стилізуйте таблицю:</p>   
-//         Введіть ширину комірки таблиці: <input type="text" style="width: 40px;" onblur="f151()"> <span class="eror1" >Некоректні дані!!!</span> 
-//        <br><br>
-//        Введіть висоту комірки таблиці: <input type="text" style="width: 40px;" onblur="f151()"> <span class="eror1" >Некоректні дані!!!</span>
-//        <br><br>
-//        
-//       Товщина: <input type="text" style="width: 40px;" onblur="f151()"><span class="eror1" >Некоректні дані!!!</span> <br><br>
-//       Тип лінії:
-//       <select  >
-//       <option  value="solid" >solid</option>
-//       <option  value="dashed">dashed</option>
-//       <option  value="double" >double</option>
-//       <option  value="dotted" >dotted</option>
-//       </select>
-//       Колір лінії:
-//       <select >
-//       <option  value="black" >black</option>
-//       <option  value="red">red</option>
-//       <option  value="blue" >blue</option>
-//       </select>
-//       <br>
-//       <br>
-//       <input type="button" value="Створити таблицю"  onclick="f15()">
-//       </form>
+function btnEdit() {
+    getId("content").style.display = "none";
+    getId("editText").style.display = "flex";
+}
+
+function createTable() {
+
+    var table = document.createElement('table'),
+        tableBorderWidth = getId('tableWidth').value + 'px', 
+        tableBorderStyle = getId('lineType').value,
+        tableBorderColor = getId('lineColor').value;
+
+
+    table.style.border = tableBorderWidth + ' ' + tableBorderStyle + ' ' + tableBorderColor;
+
+    table.style.borderSpacing = getId('cellSpacing').value + 'px';
+
+
+    for (var i = 0; i < getId('tableRow').value; i++) {
+        var tableRow = document.createElement('tr');
+
+        for (var j = 0; j < getId('tableCol').value; j++) {
+            var tableColumn = document.createElement('td');
+
+            tableColumn.style.height = getId('tableHeight').value + 'px';
+            tableColumn.style.width = getId('tableWidth').value + 'px';
+            tableColumn.style.border = tableBorderWidth + ' ' + tableBorderStyle + ' ' + tableBorderColor;
+
+            tableRow.appendChild(tableColumn);
+        }
+
+        table.appendChild(tableRow);
+    }
+
+    getId("editText").value += table.outerHTML;
+}
+
+var checker7 = false;
+function saveText() {
+        getId("content").style.display = "block";
+        getId("editText").style.display = "none";
+    getId("content").innerHTML = getId("editText").value;
+}
+
+function selectedTable() {
+    getId("box4").style.display = "block";
+    getId("box5").style.display = "none";
+}
+
+function selectedList() {
+    getId("box4").style.display = "none";
+    getId("box5").style.display = "block";
+}
+
+function createList() {
+    if (getId("listUl").checked) {
+        var ul = document.createElement("ul");
+        for (var i = 0; i < getId("elementQuantity").value; i++) {
+            var li = document.createElement('li');
+            li.innerHTML = "Sometext";
+            ul.appendChild(li);
+        }
+        getId('editText').value += ul.outerHTML;
+    } else if (getId("listOl").checked) {
+        var ol = document.createElement("ol");
+        for (var i = 0; i < getId("elementQuantity").value; i++) {
+            var li = document.createElement('li');
+            li.innerHTML = "Sometext";
+            ol.appendChild(li);
+            for (var j = 0; j < 4; j++) {
+                if (getId("markerType")[j].selected) {
+                    li.style.listStyleType = getId("markerType").value;
+                }
+            }
+        }
+        getId('editText').value += ol.outerHTML;
+    }
+}
